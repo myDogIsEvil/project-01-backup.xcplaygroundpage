12d544fe1344dec029d005e0276bdcc9fd8a1f51
michaelK <kiss-michael@hotmai.com>
2018-12-01 21:26:12 -0500

    added files

--- Contents.swift
+++ Contents.swift
@@ -1,4 +1,4 @@
-// project 01.  Date of commit (3.1): 12/01/2018
+// project 01.  Date of commit (3.2): 12/01/2018
 
 /*
  Michael Kiss.
@@ -35,44 +35,49 @@
 
  */
 // step 1: populate my players array with the above info
+// changed "playerExp" to "isPlayerExperienced" to meet coding naming convention
 let players: [[String: Any]] = [
-    ["playerName": "Joe Smith", "playerHeight": 42.0, "playerExp": true, "playerGuardian": "Jim and Jan Smith"],
-    ["playerName": "Jill Tanner", "playerHeight": 36.0, "playerExp": true, "playerGuardian": "Clara Tanner"],
-    ["playerName": "Bill Bon", "playerHeight": 43.0, "playerExp": true, "playerGuardian": "Sara and Jenny Bon"],
-    ["playerName": "Eva Gordon", "playerHeight": 45.0, "playerExp": false, "playerGuardian": "Wendy and Mike Gordon"],
-    ["playerName": "Matt Gil", "playerHeight": 40.0, "playerExp": false, "playerGuardian": "Charles and Sylvia Gill"],
-    ["playerName": "Kimmy Stein", "playerHeight": 41.0, "playerExp": false, "playerGuardian": "Bill and Hillary Stein"],
-    ["playerName": "Sammy Adams", "playerHeight": 45.0, "playerExp": false, "playerGuardian": "Jeff Adams"],
-    ["playerName": "Karl Saygan", "playerHeight": 42.0, "playerExp": true, "playerGuardian": "Heather Bledsoe"],
-    ["playerName": "Suzane Greenberg", "playerHeight": 44.0, "playerExp": true, "playerGuardian": "Henrietta Dumas"],
-    ["playerName": "Sal Dali", "playerHeight": 41.0, "playerExp": false, "playerGuardian": "Gala Dali"],
-    ["playerName": "Joe Kavalier", "playerHeight": 39.0, "playerExp": false, "playerGuardian": "Sam and Elaine Kavalier"],
-    ["playerName": "Ben Finkelstein", "playerHeight": 44.0, "playerExp": false, "playerGuardian": "Aaron and Jill Finkelstein"],
-    ["playerName": "Diego Soto", "playerHeight": 41.0, "playerExp": true, "playerGuardian": "Robin and Sarika Soto"],
-    ["playerName": "Chloe Alaska", "playerHeight": 47.0, "playerExp": false, "playerGuardian": "David and Jamie Alaska"],
-    ["playerName": "Arnold Willis", "playerHeight": 43.0, "playerExp": false, "playerGuardian": "Claire Willis"],
-    ["playerName": "Phillip Helm", "playerHeight": 44.0, "playerExp": true, "playerGuardian": "Thomas Helm and Eva Jones"],
-    ["playerName": "Les Clay", "playerHeight": 42.0, "playerExp": true, "playerGuardian": "Wynonna Brown"],
-    ["playerName": "Hershel Krustofski", "playerHeight": 45.0, "playerExp": true, "playerGuardian": "Hyman and Rachel Krustofski"],
+    ["playerName": "Joe Smith", "playerHeight": 42.0, "isPlayerExperienced": true, "playerGuardian": "Jim and Jan Smith"],
+    ["playerName": "Jill Tanner", "playerHeight": 36.0, "isPlayerExperienced": true, "playerGuardian": "Clara Tanner"],
+    ["playerName": "Bill Bon", "playerHeight": 43.0, "isPlayerExperienced": true, "playerGuardian": "Sara and Jenny Bon"],
+    ["playerName": "Eva Gordon", "playerHeight": 45.0, "isPlayerExperienced": false, "playerGuardian": "Wendy and Mike Gordon"],
+    ["playerName": "Matt Gil", "playerHeight": 40.0, "isPlayerExperienced": false, "playerGuardian": "Charles and Sylvia Gill"],
+    ["playerName": "Kimmy Stein", "playerHeight": 41.0, "isPlayerExperienced": false, "playerGuardian": "Bill and Hillary Stein"],
+    ["playerName": "Sammy Adams", "playerHeight": 45.0, "isPlayerExperienced": false, "playerGuardian": "Jeff Adams"],
+    ["playerName": "Karl Saygan", "playerHeight": 42.0, "isPlayerExperienced": true, "playerGuardian": "Heather Bledsoe"],
+    ["playerName": "Suzane Greenberg", "playerHeight": 44.0, "isPlayerExperienced": true, "playerGuardian": "Henrietta Dumas"],
+    ["playerName": "Sal Dali", "playerHeight": 41.0, "isPlayerExperienced": false, "playerGuardian": "Gala Dali"],
+    ["playerName": "Joe Kavalier", "playerHeight": 39.0, "isPlayerExperienced": false, "playerGuardian": "Sam and Elaine Kavalier"],
+    ["playerName": "Ben Finkelstein", "playerHeight": 44.0, "isPlayerExperienced": false, "playerGuardian": "Aaron and Jill Finkelstein"],
+    ["playerName": "Diego Soto", "playerHeight": 41.0, "isPlayerExperienced": true, "playerGuardian": "Robin and Sarika Soto"],
+    ["playerName": "Chloe Alaska", "playerHeight": 47.0, "isPlayerExperienced": false, "playerGuardian": "David and Jamie Alaska"],
+    ["playerName": "Arnold Willis", "playerHeight": 43.0, "isPlayerExperienced": false, "playerGuardian": "Claire Willis"],
+    ["playerName": "Phillip Helm", "playerHeight": 44.0, "isPlayerExperienced": true, "playerGuardian": "Thomas Helm and Eva Jones"],
+    ["playerName": "Les Clay", "playerHeight": 42.0, "isPlayerExperienced": true, "playerGuardian": "Wynonna Brown"],
+    ["playerName": "Hershel Krustofski", "playerHeight": 45.0, "isPlayerExperienced": true, "playerGuardian": "Hyman and Rachel Krustofski"]
 ]
 players.count // this is just here to confirm that I got all the players.  18.
 
 /*
  This section divides the players up based on experienced. From the above list, each should be populated by 9 players.  Was a frustrating section.  Had all kinds of problems finding a solution that did not throw an error about Any and bool. Came across a hint in the swift ebook about using a switch statement.  Which, for reasons I do not currently understand, the compiler accepted.
  */
+/*
+    Made a few changes here from prior commit.  The moderator suggested a few changes, and I implemented them.  Originally I, let experiencePlayer = sortPlayers["playerExp"].  First I changed the variable name "playerExp" to "isPlayerExperienced". Then, when I moved the as Bool out of the interior of the switch statement and onto the switch condition, I got a couple of errors. Had to make the case an if let statement, and had to use as! not as? or as, for the compiler to accept
+*/
 
-var experiencedPlayer: [[String: Any]] = []
-var inexperiencedPlayer: [[String: Any]] = []
+var experiencedPlayers: [[String: Any]] = []
+var inexperiencedPlayers: [[String: Any]] = []
 
 for sortPlayers in players {
-    let expPlayer = sortPlayers["playerExp"]
-    switch expPlayer {
-    case true as Bool:  experiencedPlayer.append(sortPlayers)
-    default:  inexperiencedPlayer.append(sortPlayers)
+    if let experiencePlayer = sortPlayers["isPlayerExperienced"] {
+        switch experiencePlayer as! Bool {
+        case true:  experiencedPlayers.append(sortPlayers)
+        case false:  inexperiencedPlayers.append(sortPlayers)
+        }
     }
 }
-experiencedPlayer.count
-inexperiencedPlayer.count
+experiencedPlayers.count
+inexperiencedPlayers.count
 
 /*
  players have been sorted correctly.  9 in each array.  Decided to pass on extra credit, as I want to move on to the next unit before xmas!  :)   Note:  the sort just grabs them in the array's index order (though since I used for loops they are not directly referenced by index number).  The instructions only said that the teams should be fairly populated, not how to do it.  So I just took them in the order provided.
@@ -83,16 +88,16 @@ var teamDragons: [[String: Any]] = []
 var teamRaptors: [[String: Any]] = []
 var league = [teamDragons, teamSharks, teamRaptors]
 
-func teamAssignment () {
-    let experiencedPlayerCount: Int = experiencedPlayer.count / league.count
-    let inexperiencedPlayerCount: Int = players.count / league.count
+func assignTeams () {
+    let experiencedPlayerCount = experiencedPlayers.count / league.count
+    let inexperiencedPlayerCount = players.count / league.count
 
     // inexperienced players here equals max players per team.  Or if you prefer, total
     // players / teams.  Since the experienced logic runs first it should populate
     // the inexperienced with the remainder of players (10 thru 18).  This is also where I
     // would expect to capture any additional players
 
-    for player in experiencedPlayer {
+    for player in experiencedPlayers {
         if teamDragons.count != experiencedPlayerCount {
             teamDragons.append(player)
         } else if teamSharks.count != experiencedPlayerCount {
@@ -104,7 +109,7 @@ func teamAssignment () {
             break
         }
     }
-    for player in inexperiencedPlayer {
+    for player in inexperiencedPlayers {
         if inexperiencedPlayerCount != teamDragons.count {
             teamDragons.append(player)
         } else if inexperiencedPlayerCount != teamSharks.count {
@@ -117,13 +122,19 @@ func teamAssignment () {
         }
     }
 }
-teamAssignment()
+assignTeams()
 teamDragons.count
 teamSharks.count
 teamRaptors.count
-/*
- All three teams have been populated by six players. Three experienced, and three inexperienced, players aside. This last section was much harder to do than it should have been.  The joy of trying to unmask a optional variable cannot be put into words.  :)  Tried MANY different ways, but when the compiler didn't crash, the word optional was ever present in my output.  I eventually got it to work, but I admit I had to google it.  Even then it was a pain.  I found I had to combine two of the online suggestions for unmasking an optional (as neither worked by itself).  First I declared two constant variables and set them equal to the dictionary entries inside their cooresponding arrays using any?.  Then I had to use an if statement setting a new variable equal to my just established constant.  It worked.  It just got the optional out of my letter.  Not exactly happy with the letters, but I just want to finish, and put it up on gitHub.  Maybe I'll work on them before I submit.  ok... so this is what happens when you just crank out the code without reading the grading requirements first.  :)  All letters have to be stored in one array.  So I came up with a quick fix in my code.  Added a letters array of type any.  Appended the contents of my three teams letter's into the letters array.
+/*  Note: the comments here have been changed to reflect a code change suggested by thedan84.
+    The portion inside of the two [[edit]] in the text can be replaced with the text in the block comments after this block ends.  I left the original text unchanged in this comment
+ 
+ All three teams have been populated by six players. Three experienced, and three inexperienced, players aside. This last section was much harder to do than it should have been.  The joy of trying to unmask a optional variable cannot be put into words.  :)  [[edit]] Tried MANY different ways, but when the compiler didn't crash, the word optional was ever present in my output.  I eventually got it to work, but I admit I had to google it.  Even then it was a pain.  I found I had to combine two of the online suggestions for unmasking an optional (as neither worked by itself).  First I declared two constant variables and set them equal to the dictionary entries inside their cooresponding arrays using any?.  Then I had to use an if statement setting a new variable equal to my just established constant.  It worked.  It just got the optional out of my letter. [[edit]] Not exactly happy with the letters, but I just want to finish, and put it up on gitHub.  Maybe I'll work on them before I submit.  ok... so this is what happens when you just crank out the code without reading the grading requirements first.  :)  All letters have to be stored in one array.  So I came up with a quick fix in my code.  Added a letters array of type any.  Appended the contents of my three teams letter's into the letters array.
  */
+/*
+ Took moderators advice (thanks) and combined two lines of code at the top of each for loop into one line. Annoyingly this is pretty close to what I spent 2 days trying to get to work in the earliest version of this code.  It didn't work then, i think,  because I kept trying to make it work either with an && or two if let statements before the {.  I left the original lines there to show the changes.  And for myself to refer back to, if necessary, should a simiiar problem pop up in the future.  :)
+ */
+
 var dragonWelcomeLetter = ""
 var sharkWelcomeLetter = ""
 var raptorWelcomeLetter = ""
@@ -134,33 +145,34 @@ let sharkPracticeDate = "March 17"
 let sharkPracticeTime = "3 PM"
 let raptorPracticeDate = "March 18"
 let raptorPracticeTime = "1 PM"
-func letterWriter() {
+
+func writeLetters() {
     for dragonPlayers in teamDragons {
-        let dragonGauardian: Any? = dragonPlayers["playerGuardian"]
-        let dragonPlayer: Any? = dragonPlayers["playerName"]
-        if let unwrappedDragonPlayer = dragonPlayer, let unwrappedDragonGaurdian = dragonGauardian{
-            dragonWelcomeLetter =  "Hello,  \(unwrappedDragonGaurdian), your child, \(unwrappedDragonPlayer), has been selected to the Dragons!  I am your coach, Peter DeBerg.  Our initial practice will begin at \(dragonPracticeDate) on \(dragonPracticeTime)th."
+        //let dragonGuardian: Any? = dragonPlayers["playerGuardian"]
+        //let dragonPlayer: Any? = dragonPlayers["playerName"]
+        if let unwrappedDragonPlayer = dragonPlayers["playerName"], let unwrappedDragonGuardian = dragonPlayers["playerGuardian"] {
+            dragonWelcomeLetter =  "Hello,  \(unwrappedDragonGuardian), your child, \(unwrappedDragonPlayer), has been selected to the Dragons!  I am your coach, Peter DeBerg.  Our initial practice will begin at \(dragonPracticeDate) on \(dragonPracticeTime)th."
             letters.append(dragonWelcomeLetter)
         }
     }
     for sharkPlayers in teamSharks {
-        let sharkGauardian: Any? = sharkPlayers["playerGuardian"]
-        let sharkPlayer: Any? = sharkPlayers["playerName"]
-        if let unwrappedSharkPlayer = sharkPlayer, let unwrappedSharkGaurdian = sharkGauardian {
+        //let sharkGauardian: Any? = sharkPlayers["playerGuardian"]
+        //let sharkPlayer: Any? = sharkPlayers["playerName"]
+        if let unwrappedSharkPlayer = sharkPlayers["playerName"], let unwrappedSharkGaurdian = sharkPlayers["playerGuardian"] {
             sharkWelcomeLetter = "Welcome \(unwrappedSharkGaurdian), your child, \(unwrappedSharkPlayer), has been chosen to play for the Sharks!  I am your coach, Jacob Molson.  Our first practice will start at \(sharkPracticeTime) on \(sharkPracticeDate)th."
             letters.append(sharkWelcomeLetter)
         }
     }
     for raptorPlayers in teamRaptors {
-        let raptorGauardian: Any? = raptorPlayers["playerGuardian"]
-        let raptorPlayer: Any? = raptorPlayers["playerName"]
-        if let unwrappedRaptorPlayer = raptorPlayer, let unwrappedRaptorGaurdian = raptorGauardian {
+        //let raptorGauardian: Any? = raptorPlayers["playerGuardian"]
+        //let raptorPlayer: Any? = raptorPlayers["playerName"]
+        if let unwrappedRaptorPlayer = raptorPlayers["playerName"], let unwrappedRaptorGaurdian = raptorPlayers["playerGuardian"] {
             raptorWelcomeLetter = "Greetings to, \(unwrappedRaptorPlayer), and their family, \(unwrappedRaptorGaurdian). I want to take a second and welcome you to your team.  The Raptors.  We were champions last year.  I have no doubt that we will repeat this year.  I am Coach Dick Hatcher. Our practice season will begin at \(raptorPracticeTime) on \(raptorPracticeDate)th."
             letters.append(raptorWelcomeLetter)
         }
     }
 }
-letterWriter()
+writeLetters()
 letters.count
 for letter in letters {
     print(letter)
@@ -173,3 +185,7 @@ for letter in letters {
  */
 
 //  the end... of project 1
+
+
+
+
--- README.md
+++
@@ -1,7 +0,0 @@
-# project-01-backup.xcplaygroundpage
-unit 01 project
-The purpose of this project is to sort a group of soccer players into three evenly matched teams, based on their experience levels (exta 
-credit if also sort based on avgerage height.  After the players info is entered into a collection storing their info (I chose to use an
-array of dictionary), the players array are sorted into 2 new arrays of the same type based on experience.  Next the players are divided into 
-3 teams, so that each team has the same number of players (which means they have the same number of expienced and inexperienced players).  
-Lastly, a letter must be generated and printed to each players guardian welcoming them to that team.
